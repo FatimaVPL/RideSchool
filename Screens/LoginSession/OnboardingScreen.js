10eaa9fc77baa7ce163c982bf61c2df5c353314a
fatiV <perezlarafatima@gmail.com>
2023-08-16 08:27:28 -0600

    Mejoras pantallas

--- Screens/LoginSession/OnboardingScreen.js
+++ Screens/LoginSession/OnboardingScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useState, useRef, useCallback, useEffect } from "react";
 import {
    View,
    Text,
@@ -9,7 +9,8 @@ import {
    StyleSheet,
    KeyboardAvoidingView,
    Platform,
-   Animated
+   Animated, 
+   Keyboard,
 } from "react-native"
 import { TextInput } from 'react-native-paper'
 import Lottie from 'lottie-react-native';
@@ -27,7 +28,7 @@ const dumyAlumnos = [
 ]
 function sleep(ms) {
    return new Promise(resolve => setTimeout(resolve, ms));
- }
+}
 
 async function dumyBuscarAlumno(noControl) {
    await sleep(2000)
@@ -46,22 +47,45 @@ const OnboardingScreen = ({ navigation }) => {
    const [formData, setFormData] = useState({})
    const [selectedScreen, setSelectedScreen] = useState(0)
    const [loading, setLoading] = useState(false)
-   
+
    const scrollX = useRef(new Animated.Value(0)).current
    const slidesRef = useRef(null)
+
+   const [keyboardVisible, setKeyboardVisible] = useState(false)
+
+
+   useEffect(() => {
+      // Scroll to the selected screen on load
+      const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+        setKeyboardVisible(true);
+      });
+      const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+        setKeyboardVisible(false);
+      });
+    
+      return () => {
+        keyboardDidShowListener.remove();
+        keyboardDidHideListener.remove();
+      };
+    }, []);
+    
+
+    const [selectedRole, setSelectedRole] = useState(null);
 
    const slides = [
       {
          id: 1,
-         title: 'Como planeas usar RideSchool ?',
-         info: 'Con RideSchool, podrás ofrecer ride\'s a otros estudiantes o solicitarlos tu? Puedes cambiar tus preferencias más tarde desde tu perfil.',
+        /* title: '¿Cómo planeas usar RideSchool?',
+         info: 'Con RideSchool, podrás ofrecer rides a otros estudiantes o solicitarlos tú? Puedes cambiar tus preferencias más tarde desde tu perfil.',*/
+         title: 'Elije uno de los roles, para comenzar',
+         info: 'Puedes cambiar tu rol más tarde desde tu perfil.',
          svg: <Lottie source={require('../../assets/LottieFiles/passagerOrCar.json')} />,
          options: [{ label: "Pasajero", value: "pasajero" }, { label: "Chofer", value: "chofer" }]
       },
       {
          id: 2,
          title: 'Ingresa tu número de control',
-         info: 'Por motivos de seguridad, necesitamos verificar que eres un estudiante de la escuela.',
+         info: 'Por motivos de seguridad, necesitamos verificar que eres un estudiante vigente.',
          svg: <Lottie source={require('../../assets/LottieFiles/Credentials.json')} />,
          input: [{ atr: "noControl" }]
       },
@@ -88,43 +112,48 @@ const OnboardingScreen = ({ navigation }) => {
    /* Pasar a la siguiente pantalla */
    const scrollTo = async () => {
       try {
-         setLoading(true)
-         // No es la ultima pantalla
+         setLoading(true);
+         if (inputRefs.current[selectedScreen]) {
+           inputRefs.current[selectedScreen].blur(); // Desenfocar el campo de entrada actual
+         }
          if (selectedScreen < slides.length - 1) {
-            if (selectedScreen === 2) {
-               // TODO : Verificar que sea un alumno
-               await dumyBuscarAlumno(formData.noControl.toLowerCase().trim())
-            }
-            slidesRef.current.scrollToIndex({ index: selectedScreen + 1 })
-            setSelectedScreen(p => p + 1)
+           if (selectedScreen === 2) {
+             // TODO: Verificar que sea un alumno
+             await dumyBuscarAlumno(formData.noControl.toLowerCase().trim());
+           }
+           slidesRef.current.scrollToIndex({ index: selectedScreen + 1 });
+           setSelectedScreen(p => p + 1);
+           if (inputRefs.current[selectedScreen + 1]) {
+             inputRefs.current[selectedScreen + 1].focus();
+           }
+         } else {
+           // Resto de tu código
          }
-         else {
-            // Crear Un Registro
-            console.log(formData)
-            setUsage()
-            registerUser({
-               email: formData.noControl.toLowerCase().trim() + "@alumnos.itsur.edu.mx",
-               password: formData.password,
-               role: formData.role,
-               firstName: dumyAlumnos.find(almno => almno.noControl === formData.noControl.toLowerCase().trim()).nombre
-            })
-         }
-      } catch (err) {
-         alert(err)
-      } finally {
-         setLoading(false)
-      }
+       } catch (err) {
+         alert(err);
+       } finally {
+         setLoading(false);
+       }
    }
 
    const Screen = ({ item }) => {
       const { title, info, svg, options, input } = item
 
       return (
+         <>
          <View style={[{ width }, { display: 'flex' }]}>
-            <View style={{
-               flex: 1,
-               flexDirection: 'column',
-            }}>
+         <StatusBar
+        animated={true}
+        barStyle={'light'}
+      />
+      <View style={{
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        color: colors.text,
+        position: 'relative',
+        paddingBottom: keyboardVisible ? 300 : 0, // Ajusta este valor según sea necesario
+      }}>
 
                <View style={{
                   display: 'flex',
@@ -133,7 +162,8 @@ const OnboardingScreen = ({ navigation }) => {
                   alignItems: 'center',
                   marginTop: 20,
                }}>
-                  <View style={[{ width }, { display: 'flex', height: '100%' }]}>
+                  <View style={[
+                     { width }, { display: 'flex', height: '100%' }]}>
                      {svg}
                   </View>
                </View>
@@ -173,32 +203,53 @@ const OnboardingScreen = ({ navigation }) => {
                      options &&
                      <View style={{ display: 'flex' }}>
                         {options.map((option, indx) =>
-                           <TouchableOpacity
-                              key={`${option.value}-${indx}`}
-                              style={formData?.role === option.value ? styles.selectedOption : styles.option}
-                              onPress={() => handleSelectRole(option.value)}>
-                              <Text>
-                                 {option.label}
-                              </Text>
-                           </TouchableOpacity>)}
+                     <TouchableOpacity
+                        key={`${option.value}-${indx}`}
+                        style={formData?.role === option.value ? styles.selectedOption : styles.option}
+                        onPress={() => handleSelectRole(option.value)}>
+                        <Text style={{ color: 'white',  fontWeight: 'bold', }}>{/* Updated color to white */}
+                           {option.label}
+                        </Text>
+                     </TouchableOpacity>)}
                      </View>
                   }
                   {
                      input &&
                      <View style={{}}>
                         {input.map((field, indx) =>
-                           <TextInput
-                              //ref={inputRefs.current[field.atr]}
-                              key={`${field.atr}-${indx}`}
-                              value={formData[field.atr]}
-                              onChangeText={(text) => handleChangeText(field.atr, text)}
-                           >
-                           </TextInput>)}
+                      <TextInput
+                      // ref={inputRefs.current[field.atr]}
+                      style={{
+                        width: 300,
+                        height: 50,
+                        backgroundColor: 'white',
+                        borderRadius: 8,
+                        shadowColor: '#000',
+                        shadowOffset: {
+                          width: 0,
+                          height: 2,
+                        },
+                        shadowOpacity: 0.25,
+                        shadowRadius: 3.84,
+                        elevation: 5,
+                        paddingHorizontal: 16,
+                        marginTop: 10,
+                      }}
+                      key={`${field.atr}-${indx}`}
+                      value={formData[field.atr]}
+                      onChangeText={(text) => handleChangeText(field.atr, text)}
+                      placeholder="Escribe aquí"
+                      placeholderTextColor="#888"
+                      autoFocus={true}
+                    />
+                    
+                      )}
                      </View>
                   }
                </View>
             </View>
          </View>
+         </>
       )
    }
 
@@ -217,7 +268,7 @@ const OnboardingScreen = ({ navigation }) => {
          }}>
 
             {/*  Slides  */}
-            <KeyboardAvoidingView style={{ width: '100%', height: '80%' }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <KeyboardAvoidingView style={{ width: '100%', height: '90%' }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
                <View style={{ flex: 1 }}>
                   <FlatList
                      ref={slidesRef}
@@ -245,7 +296,7 @@ const OnboardingScreen = ({ navigation }) => {
                {/* Indicador de Pagina */}
                <View style={{
                   width: '100%',
-                  height: 50,
+                  height: 20,
                   display: 'flex',
                   flexDirection: 'row',
                   justifyContent: 'center',
@@ -282,10 +333,10 @@ const OnboardingScreen = ({ navigation }) => {
                   flex: 1,
                   display: 'flex',
                   flexDirection: 'row',
-                  paddingHorizontal: 30,
-                  paddingTop: 10,
-                  paddingBottom: 25,
+                  paddingHorizontal: 35,
+                  paddingBottom: 10,
                   display: 'flex',
+                  
                }}>
                   <TouchableOpacity
                      // TODO: Styles disabled
@@ -318,23 +369,35 @@ export default OnboardingScreen
 
 const styles = StyleSheet.create({
    option: {
-      width: '100%',
-      height: 50,
-      backgroundColor: "#F0F0F0",
+      marginTop: 10,
+      backgroundColor: '#FCD85E', //por definir en dark
+      padding: 10,
       borderRadius: 10,
-      display: 'flex',
-      justifyContent: 'center',
+      shadowColor: "#000", //por definir en dark
+      shadowOffset: {
+         width: 0,
+         height: 3,
+      },
+      shadowOpacity: 0.27,
+      shadowRadius: 4.65,
+      elevation: 6,
+      width: 150,
       alignItems: 'center',
-      marginVertical: 5,
    },
    selectedOption: {
-      width: '100%',
-      height: 50,
-      backgroundColor: "#e6e6e6",
+      marginTop: 10,
+      backgroundColor: '#CBA524', //por definir en dark
+      padding: 10,
       borderRadius: 10,
-      display: 'flex',
-      justifyContent: 'center',
+      shadowColor: "#000", //por definir en dark
+      shadowOffset: {
+         width: 0,
+         height: 3,
+      },
+      shadowOpacity: 0.27,
+      shadowRadius: 4.65,
+      elevation: 6,
+      width: 150,
       alignItems: 'center',
-      marginVertical: 5,
    }
 })
