3094f69355fe44b3f97d5e7dc8a9a1313a728d9d
Gaby_Perez <monseperez267@gmail.com>
2023-11-13 09:08:57 -0600

    Notificaciones

--- App.js
+++ App.js
@@ -6,20 +6,23 @@ import Main from './Main';
 import { ThemeProvider } from './hooks/ThemeContext';
 import { PaperProvider } from 'react-native-paper';
 import { ChatProvider } from './context/ChatContext';
+import { NotificationProvider } from './context/NotificationsContext';
 
 export default function App() {
   //registerNNPushToken(13000, 'Dke2V9YbViRt26fTH2Mv7q');
-  registerNNPushToken(14050, 'teVYjQw7P4lRK3FcQSIuzV');
+  //registerNNPushToken(14050, 'teVYjQw7P4lRK3FcQSIuzV');
 
   return (
     <NavigationContainer>
       <AuthProvider>
         <ThemeProvider>
-          <ChatProvider>
-            <PaperProvider>
-              <Main />
-            </PaperProvider>
-          </ChatProvider>
+          <NotificationProvider>
+            <ChatProvider>
+              <PaperProvider>
+                <Main />
+              </PaperProvider>
+            </ChatProvider>
+          </NotificationProvider>
         </ThemeProvider>
       </AuthProvider>
     </NavigationContainer>
